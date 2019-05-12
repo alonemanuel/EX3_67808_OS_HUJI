aaf0fa055fe3ae3dd2ed50a491992deecd889988
Alon Derfner <alonemanuel@cs.huji.ac.il>
2019-05-12 20:31:58 +0300

    Tarantino passed up to line separators

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -30,7 +30,7 @@ typedef struct ThreadContext
 	// Number of thread given upon initialization.
 	int threadNum;
 	// Input vector given by client.
-	InputVec *inputVec;
+	InputVec inputVec;
 	// Intermediate vector.
 	vector<pair<K2 *, V2 *>> *interVec;
 	// Semaphore.
@@ -47,12 +47,12 @@ typedef struct ThreadContext
 	pthread_mutex_t *mutex;
 
 	// Ctor.
-	ThreadContext(JobContext *_jobContext, int _threadNum, InputVec * _inputVec, sem_t *_sem,
-	        std::atomic<int> *_atomicCounter, OutputVec * _outputVec, const MapReduceClient *_client,
-	        Barrier *_barrier, pthread_mutex_t* _mutex) :
-			    jobContext(_jobContext), threadNum(_threadNum), inputVec(_inputVec),
-			    interVec(new vector<IntermediatePair>()), sem(_sem), atomicCounter(_atomicCounter),
-			    outputVec(_outputVec), client(_client), barrier(_barrier), mutex(_mutex)
+	ThreadContext(JobContext *_jobContext, int _threadNum, const InputVec &_inputVec, sem_t *_sem,
+				  std::atomic<int> *_atomicCounter, OutputVec &_outputVec, const MapReduceClient *_client,
+				  Barrier *_barrier, pthread_mutex_t *_mutex) :
+			jobContext(_jobContext), threadNum(_threadNum), inputVec(_inputVec),
+			interVec(new vector<IntermediatePair>()), sem(_sem), atomicCounter(_atomicCounter),
+			outputVec(&_outputVec), client(_client), barrier(_barrier), mutex(_mutex)
 	{}
 
 } ThreadContext;
@@ -62,30 +62,32 @@ typedef struct ThreadContext
  */
 typedef struct JobContext
 {
-    // Vector of threads alive within this job.
-    vector<ThreadContext> *threads{};
-    // State of the current job.
-    JobState *state;
+	// Vector of threads alive within this job.
+	vector<ThreadContext *> *threads{};
+	// State of the current job.
+	JobState *state;
 
-    // Ctor for a JobContext instance. Receives _threads as pointer.
-    JobContext(vector<ThreadContext> *_threads) : threads(_threads)
-    {
-        // Inits state.
-        state = new JobState();
-        // Sets state.
-        state->stage = UNDEFINED_STAGE;
-        state->percentage = 0;
-    }
+	// Ctor for a JobContext instance. Receives _threads as pointer.
+	JobContext(vector<ThreadContext *> *_threads) : threads(_threads)
+	{
+		// Inits state.
+		state = new JobState();
+		// Sets state.
+		state->stage = UNDEFINED_STAGE;
+		state->percentage = 0;
+	}
 
-    ~JobContext() {
-        if(!threads->empty()){
-            delete threads->back().barrier;
-            delete threads->back().mutex;
-            delete threads->back().atomicCounter;
-            delete threads->back().sem;
-        }
-        delete threads;
-    }
+	~JobContext()
+	{
+		if (!threads->empty())
+		{
+			delete threads->back()->barrier;
+			delete threads->back()->mutex;
+			delete threads->back()->atomicCounter;
+			delete threads->back()->sem;
+		}
+		delete threads;
+	}
 } JobContext;
 
 
@@ -100,9 +102,12 @@ void emit2(K2 *key, V2 *value, void *context)
 
 void emit3(K3 *key, V3 *value, void *context)
 {
-    auto curr_context = (ThreadContext *) context;
-    auto p = OutputPair(key, value);
-    curr_context->outputVec->push_back(p);
+	auto curr_context = (ThreadContext *) context;
+	auto p = OutputPair(key, value);
+	curr_context->outputVec->push_back(p);
+	cout << LOG_PREFIX << "(Inside threadmapred) Output vec is now of size: " << curr_context->outputVec->size()
+		 << endl;
+
 }
 
 void mapPhase(ThreadContext *context)
@@ -110,16 +115,16 @@ void mapPhase(ThreadContext *context)
 	cout << LOG_PREFIX << "Starting map phase for thread " << context->threadNum << endl;
 	context->jobContext->state->stage = MAP_STAGE;
 	vector<pair<K2 *, V2 *>> *interVec = context->interVec;
-    int oldValue;
+	int oldValue;
 	// Use atomic to avoid race conditions.
-	while (context->atomicCounter->load() < context->inputVec->size())
+	while (context->atomicCounter->load() < context->inputVec.size())
 	{
 		oldValue = (*(context->atomicCounter))++;
-		std::pair<K1 *, V1 *> currPair = context->inputVec->at(static_cast<unsigned int>(oldValue));
+		std::pair<K1 *, V1 *> currPair = context->inputVec.at(static_cast<unsigned int>(oldValue));
 		// Map each pair.
 		context->client->map(currPair.first, currPair.second, interVec);
 		// Update percentage.
-		context->jobContext->state->percentage = ((oldValue + 1) / (float) context->inputVec->size() * 100);
+		context->jobContext->state->percentage = ((oldValue + 1) / (float) context->inputVec.size() * 100);
 	}
 }
 
@@ -130,14 +135,14 @@ void sortPhase(ThreadContext *context)
 
 void shufflePhase(vector<vector<pair<K2 *, V2 *>>> *reduceQueue, ThreadContext *context)
 {
-    context->jobContext->state->stage = REDUCE_STAGE;
-    cout << LOG_PREFIX << "Starting shuffle phase." << endl;
+	context->jobContext->state->stage = REDUCE_STAGE;
+	cout << LOG_PREFIX << "Starting shuffle phase." << endl;
 
 	// Throw all pairs into a single vector.
 	auto *newInter = new vector<pair<K2 *, V2 *>>();
-	for (ThreadContext tc:*context->jobContext->threads)
+	for (ThreadContext *tc:*context->jobContext->threads)
 	{
-		newInter->insert(newInter->begin(), tc.interVec->begin(), tc.interVec->end());
+		newInter->insert(newInter->begin(), tc->interVec->begin(), tc->interVec->end());
 	}
 	std::sort(newInter->begin(), newInter->end(), comparePtrToPair);
 
@@ -149,16 +154,18 @@ void shufflePhase(vector<vector<pair<K2 *, V2 *>>> *reduceQueue, ThreadContext *
 	{
 		if (*currPair->first < *k2max)
 		{
-            if (pthread_mutex_lock(context->mutex) != 0) {
-                fprintf(stderr, "Shuffle: error on pthread_mutex_lock");
-                exit(1);
-            }
+			if (pthread_mutex_lock(context->mutex) != 0)
+			{
+				fprintf(stderr, "Shuffle: error on pthread_mutex_lock");
+				exit(1);
+			}
 			reduceQueue->push_back(*currVec);
 			sem_post(context->sem);
-            if (pthread_mutex_unlock(context->mutex) != 0) {
-                fprintf(stderr, "Shuffle: error on pthread_mutex_unlock");
-                exit(1);
-            }
+			if (pthread_mutex_unlock(context->mutex) != 0)
+			{
+				fprintf(stderr, "Shuffle: error on pthread_mutex_unlock");
+				exit(1);
+			}
 			currVec = new vector<pair<K2 *, V2 *>>();
 			k2max = currPair->first;
 		}
@@ -166,6 +173,24 @@ void shufflePhase(vector<vector<pair<K2 *, V2 *>>> *reduceQueue, ThreadContext *
 		newInter->pop_back();
 		currPair = &newInter->back();
 	}
+
+	if (pthread_mutex_lock(context->mutex) != 0)
+	{
+		fprintf(stderr, "Shuffle: error on pthread_mutex_lock");
+		exit(1);
+	}
+	if (!currVec->empty())
+	{
+		reduceQueue->push_back(*currVec);
+		sem_post(context->sem);
+
+	}
+	if (pthread_mutex_unlock(context->mutex) != 0)
+	{
+		fprintf(stderr, "Shuffle: error on pthread_mutex_unlock");
+		exit(1);
+	}
+
 }
 
 void reducePhase(vector<vector<pair<K2 *, V2 *>>> *reduceQueue, int reduceSize, ThreadContext *context)
@@ -174,24 +199,29 @@ void reducePhase(vector<vector<pair<K2 *, V2 *>>> *reduceQueue, int reduceSize, 
 	{
 		sem_wait(context->sem);
 
-        if (pthread_mutex_lock(context->mutex) != 0) {
-            fprintf(stderr, "Reduce: error on pthread_mutex_lock");
-            exit(1);
-        }
+		if (pthread_mutex_lock(context->mutex) != 0)
+		{
+			fprintf(stderr, "Reduce: error on pthread_mutex_lock");
+			exit(1);
+		}
 
-        if (!reduceQueue->empty()) {
-            context->client->reduce(&reduceQueue->back(), context);
-            reduceQueue->pop_back();
-            context->jobContext->state->percentage = (reduceSize - reduceQueue->size()) / (float) reduceSize * 100;
-        } else {
-            sem_post(context->sem);
-        }
+		if (!reduceQueue->empty())
+		{
+			context->client->reduce(&reduceQueue->back(), context);
+			reduceQueue->pop_back();
+			context->jobContext->state->percentage = (reduceSize - reduceQueue->size()) / (float) reduceSize * 100;
+		}
+		else
+		{
+			sem_post(context->sem);
+		}
 
-        if (pthread_mutex_unlock(context->mutex) != 0) {
-            fprintf(stderr, "Reduce: error on pthread_mutex_lock");
-            exit(1);
-        }
-    }
+		if (pthread_mutex_unlock(context->mutex) != 0)
+		{
+			fprintf(stderr, "Reduce: error on pthread_mutex_lock");
+			exit(1);
+		}
+	}
 }
 
 /**
@@ -208,55 +238,57 @@ void threadMapReduce(ThreadContext *context)
 		shufflePhase(reduceQueue, context);
 	}
 	reducePhase(reduceQueue, reduceQueue->size(), context);
+//	cout << LOG_PREFIX << "(Inside threadmapred) Output vec is now of size: " << context->outputVec.size()<<endl;
 }
 
 JobHandle startMapReduceJob(const MapReduceClient &client, const InputVec &inputVec, OutputVec &outputVec,
-				  int multiThreadLevel)
+							int multiThreadLevel)
 {
 	cout << endl << LOG_PREFIX << "Starting job on input of size " << inputVec.size() << endl;
 	// TODO: should new be called on the semaphore?
-    auto *sem = new sem_t();
+	auto *sem = new sem_t();
 	sem_init(sem, multiThreadLevel, 0);
 
 	// atomic counter to be used as input vec index.
 	auto *atomic_counter = new std::atomic<int>(0);
 	pthread_t threadArr[multiThreadLevel];
-	auto *threads = new vector<ThreadContext>();
+	auto *threads = new vector<ThreadContext *>();
 	auto *jobContext = new JobContext(threads);
 	cout << LOG_PREFIX << "Created threads struct with " << multiThreadLevel << " threads" << endl;
 	auto *barrier = new Barrier(multiThreadLevel);
 	cout << LOG_PREFIX << "Expected work load of each thread is ~"
 		 << (int) inputVec.size() / multiThreadLevel << endl;
-    auto *mutex = new pthread_mutex_t(PTHREAD_MUTEX_INITIALIZER);
+	auto *mutex = new pthread_mutex_t(PTHREAD_MUTEX_INITIALIZER);
 	for (int i = 0; i < multiThreadLevel; ++i)
 	{
 		ThreadContext *context = new ThreadContext(jobContext, i, inputVec, sem,
 												   atomic_counter, outputVec, &client,
 												   barrier, mutex);
-		threads->push_back(*context);    // TODO: Should I pass pointer instead?
+		threads->push_back(context);    // TODO: Should I pass pointer instead?
 		pthread_create(threadArr + i, nullptr, (void *(*)(void *)) threadMapReduce, context);
 		cout << LOG_PREFIX << "Created context and thread " << i << endl;
 	}
+//	cout << LOG_PREFIX << "(Inside startmapred) Output vec is now of size: " << outputVec.size()<<endl;
 	return jobContext;    // TODO: should threads be allocated with alloc?
 }
 
 void waitForJob(JobHandle job)
 {
-    auto *jobContext = (JobContext *) job;
-    while (jobContext->state->stage != REDUCE_STAGE || jobContext->state->percentage < 100)
+	auto *jobContext = (JobContext *) job;
+	while (jobContext->state->stage != REDUCE_STAGE || jobContext->state->percentage < 100)
 	{
-    }
+	}
 }
 
 void getJobState(JobHandle job, JobState *state)
 {
-    auto *jc = (JobContext *) job;
+	auto *jc = (JobContext *) job;
 	*state = *jc->state;
 }
 
 void closeJobHandle(JobHandle job)
 {
-    waitForJob(job);
-    auto jobContext = (JobContext *) job;
-    delete jobContext;
+	waitForJob(job);
+	auto jobContext = (JobContext *) job;
+	delete jobContext;
 }
--- Tests/Tarantino/WordFrequencies.cpp
+++ Tests/Tarantino/WordFrequencies.cpp
@@ -4,6 +4,7 @@
 
 #include "WordFrequenciesClient.hpp"
 #include <algorithm>
+//#include "../../Tests/Tarantino/scripts"
 
 using std::cout;
 using std::endl;
@@ -15,8 +16,7 @@ using std::ofstream;
 using std::vector;
 
 // TODO: Play with this path to match your directories
-const string SCRIPTS_DIR = "C:\\Users\\kalir\\Desktop\\Univ\\Year 2\\Semester B\\OS\\ex3-67808-os-huji\\"
-						   "Tests\\Tarantino\\scripts\\";
+const string SCRIPTS_DIR = "/cs/usr/alonemanuel/Year2/Semester2/67808_OS/EX3_67808_OS_HUJI/Tests/Tarantino/scripts/";
 const int REPORT_FREQ_MS = 500;
 
 class ScriptJob
@@ -97,11 +97,12 @@ public:
 
 	void writeByFrequency()
 	{
+		cout << "Logging: " << "Output size is: " << output.size() << endl;
 		// get length of longest word (so we can write to the file in a nice format)
 		unsigned int maxLength = 0;
 		for (auto &frequencie : output)
 		{
-            auto length = static_cast<unsigned int>((*(Word *) frequencie.first).getWord().length());
+			auto length = static_cast<unsigned int>((*(Word *) frequencie.first).getWord().length());
 			maxLength = length > maxLength ? length : maxLength;
 		}
 
@@ -121,7 +122,7 @@ public:
 				 return ((Integer *) o1.second)->val > ((Integer *) o2.second)->val
 						|| ((Word *) o1.first)->getWord() < ((Word *) o2.first)->getWord();
 			 }
-		);
+			);
 
 		// Writ results to file
 		for (auto &pair : output)
@@ -139,6 +140,7 @@ public:
 			delete pair.first;
 			delete pair.second;
 		}
+		cout << "Logging: " << "Last pair is: " << ((Word*)output.back().first)->getWord() << endl;
 	}
 };
 
@@ -148,8 +150,8 @@ int main()
 	vector<string> paths;
 
 	paths.emplace_back(SCRIPTS_DIR + "Inglourious_Basterds");
-    paths.emplace_back(SCRIPTS_DIR + "Reservoir_Dogs");
-    paths.emplace_back(SCRIPTS_DIR + "Pulp_Fiction");
+	paths.emplace_back(SCRIPTS_DIR + "Reservoir_Dogs");
+	paths.emplace_back(SCRIPTS_DIR + "Pulp_Fiction");
 
 	// Generate jobs
 	jobs.reserve(paths.size());
@@ -186,11 +188,11 @@ int main()
 		waitForJob(job->handle);
 	}
 
-    // Write output
-    for (auto job :jobs)
+	// Write output
+	for (auto job :jobs)
 	{
-        job->writeByFrequency();
-    }
+		job->writeByFrequency();
+	}
 
 	// Kill
 	for (auto job :jobs)
